449e1dcdd4c71e513e8e3795c9cfa3d987e0279e
Giacomo Marolla <giacomo.marolla@gmail.com>
2016-05-23 13:19:57 +0200

    primo commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Giacomo Marolla" />
+        <title>Test Instilla</title>
+        <link rel="stylesheet" type="text/css" href="./css/style.css" />
+    </head>
+    <body>
+
+        <div>
+            <h1>
+                 Inserisci le url delle homepage di due siti web
+            </h1>
+            <form class="" action="index.php" method="post">
+                <input type="url" name="url1" value="">
+                <input type="url" name="url2" value="">
+                </br>
+                <input type="submit" name="FindAndCompare" value="trova e confronta">
+            </form>
+        </div>
+
+    </body>
+</html>
